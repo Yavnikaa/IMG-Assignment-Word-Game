b5c2552615c7bcec872ab932f275d004943873d4
Yavnikaa <56485761+Yavnikaa@users.noreply.github.com>
2020-03-10 10:37:23 +0530

    Add files via upload

---
+++ word-game.js
@@ -0,0 +1,55 @@
+var dictionary = [
+    "ACT",
+    "TIGER",
+    "LION",
+    "BEAR",
+    "HAPPY",
+    "JAVASCRIPT",
+    "LAB",
+    "MONEY"
+];
+var myWord;
+
+function scramble(word) {
+    return word.split('').sort(function(){ return Math.random() - 0.5 }).join('');
+}
+
+function chooseWord() {
+    return dictionary[Math.floor(Math.random() * dictionary.length)];
+}
+
+function printWord(word) {
+    var html = "";
+    for (var i = 0; i < word.length; i++) {
+        html += '<li class="tile">' + word[i] + '</li>'
+    }
+    $('.word').html(html);
+}
+
+function startGame() {
+    myWord = chooseWord();
+    
+    var scrambled = scramble(myWord);
+    
+    while (scrambled == myWord) {
+        scrambled = scramble(myWord);
+    }
+    
+    printWord(scramble(myWord));
+}
+
+$(function(){    
+    startGame();
+    
+    $('.word').sortable({
+        update: function (e, ui) {
+            console.log(myWord, $('.tile').text());
+            if (myWord === $('.tile').text()) {
+                var playAgain = confirm("That is correct. Next word?");
+                if (playAgain) {
+                    startGame();
+                }                    
+            }
+        }
+    });  
+});
